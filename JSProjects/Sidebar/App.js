3ea6f7adf4c70fafbbe1bc394154c8f31d98eaec
BMP-Dev <48845124+BMP-Dev@users.noreply.github.com>
2020-10-30 11:29:07 -0600

    Fixed Spelling error

--- JSProjects/Sidebar/App.js
+++ JSProjects/Sidebar/App.js
@@ -1,10 +1,10 @@
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
-const closBtn = document.querySelector('.sidebar');
+const closBtn = document.querySelector('.close-btn');
 sidebarToggle.addEventListener('click',()=>{
 
   sidebar.classList.toggle("show-sidebar");
 });
 closBtn.addEventListener('click',()=>{
-  sidebar.classList.remove(" show-sidebar");
+  sidebar.classList.remove("show-sidebar");
 });
